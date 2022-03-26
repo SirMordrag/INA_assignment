937df847e611302f3ca43111ab6ab7e9d8ae6261
Voska Vojtech <vojtech.voska@comap-control.com>
2022-03-26 14:49:42 +0100

    WIP: Implement Indirect Close Loop Kalman filtering

--- Navigation_solution.m
+++ Navigation_solution.m
@@ -19,13 +19,25 @@ data_length = data_length(1);
 % TEST: data length = * freq * seconds * MINUTES
 data_length = 200 * 60 * 1.5
 
-%% initialise
+%% declare, initialise
+% strapdown
 [P, V, DCM] = initial_conditions(data_acc, data_gpsP, data_gpsV, 60);
 [R_M, R_N, g_N] = get_pseudo_constants(P(1), P(2), P(3));
 r = [0 0 0]; % dunno
 kd = 1; % dunno
 F63 = 1; % dunno
-
+f_correction = 0;
+w_correction = 0;
+% KF stuff
+dx = zeros(12,1); % state error vector
+ddx = zeros(12,1); % state error vector change
+% todo reaaaaaly not zeros/ones
+P_kf = ones(12);
+Q_kf = zeros(12);
+K_kf = zeros(12, 6); % WARNING dimensions (nx x nz)
+H_kf = ones(6, 12); % ditto (nz x nx)
+R_kf = zeros(6);
+% auxiliary
 SAVED_DATA = zeros(data_length, 9);
 
 %% MAIN LOOP
@@ -35,9 +47,9 @@ for index = 1:data_length
     w = data_gyro(index,:).' * 0.0174532925; % deg/s to rad/s
 
     %% B: MEASUREMENT CORRECTIONS
-    % TODO
-    f = f;
-    w = w;
+    % WARNING signs (!)
+    f = f - f_correction;
+    w = w - w_correction;
 
     %% C: NAVIGATION MECHANISATION
     % update CDM
@@ -64,31 +76,47 @@ for index = 1:data_length
     if ~isnan(p_gps(1)) % GPS data are not NaN, good to go for whole cycle
         %% E: DELTA MEASUREMENT VECTOR
         % ideally, those two should be zero
+        % WARNING: signs / substraction order, dimensions
         delta_P = p_gps - P;
         delta_V = v_gps - V;
+        z_kf = [delta_P.'  delta_V.'  0 0 0   0 0 0   0 0 0 ]; % dunno rho and dxa and dxg
 
         %% F: CORRECTION UPDATE (KF)
         % Kalman Gain
-
-        % innovation
+        K_kf = P_kf * H_kf.' * 1/(H_kf * P_kf * H_kf.' + R_kf);
 
         % update estimate
+        innovation_kf = z_kf - H_kf * dx_next;
+        dx = dx_next + K_kf * innovation_kf;
 
         % update estimate uncertainty
+        P_kf = (eye(12) - K_kf *  H_kf) * P_kf * (eye(12) - K_kf * H_kf).' + K_kf * R_kf * K_kf.';
 
         %% G: CORRECTION APPLICATION
+        % correction factors for IMU measurements, applied then in state A
+        f_correction = 0; % TODO
+        w_correction = 0; % TODO
 
+        % apply corrections to Position, Velocity, DCM immediately
+        P = P - dx(1:3);
+        V = V - dx(4:6);
+        DCM = DCM - angle2dcm(dx(9), dx(8), dx(7), "ZYX");
 
         %% H: STATE VECTOR RESET
-
+        % WARNING: dimensions
+        % WARNING: maybe not necessary, depends on state E implementation (I guess)
+        dx = zeros(12, 1);
 
         %% I: TIME UPDATE (KF)
         % matrices
         [F, G] = get_model_matrices(P, V, DCM, f_n, w, r, kd, F63, R_M, R_N);
 
         % state extrapolation
+        % TODO u or q
+        dx_next = F * dx + G * u;
 
         % uncertainty extrapolation
+        P_kf = P_kf + Q_kf;
 
     end
 
